e73c7e0db734ff215a4f4a38eda2b4abfc3c4ad7
ChangJoo Park <changjoo.park@partner.so>
2017-01-30 15:38:23 +0900

    플랫폼 쿼리 추가

--- app.js
+++ app.js
@@ -7,27 +7,33 @@ const app = express();
 
 app.use(require('morgan')('dev'));
 
-app.use('/updates/releases', express.static(path.join(__dirname, 'releases')));
+app.use('/updates/releases/', express.static(path.join(__dirname, 'releases')));
 
 app.get('/updates/latest', (req, res) => {
   console.log('[/updates/latest] START')
-  const latest = getLatestRelease();
-  console.log('[/updates/latest] latest : ', latest);
   const clientVersion = req.query.v;
+  const clientPlatform = req.query.p;
+  const latest = getLatestRelease(clientPlatform);
+  console.log('[/updates/latest] latest : ', latest);
   console.log('[/updates/latest] clientVersion : ', clientVersion);
+  console.log('[/updates/latest] clientVersion : ', clientVersion);
+  console.log('[/updates/latest] clientPlatform : ', clientPlatform);
+  
+  // 현재 클라이언트 버전과 최신버전이 동일하면 종료
+  // 다르면 최신 버전 URL을 전달
   if(clientVersion === latest) { 
     res.status(204).end();
   } else {
     res.json({
-      url: `${getBaseUrl()}/releases/darwin/${latest}/MyApp.zip`
+      url: `${getBaseUrl()}/releases/${clientPlatform}/${latest}/MyApp.zip`
     });
   }
 });
 
 
-let getLatestRelease = () => {
+let getLatestRelease = (platform) => {
   console.log('[#getLatestRelease] START')
-  const dir = `${__dirname}/releases/darwin`;
+  const dir = `${__dirname}/releases/${platform}`;
   console.log('[#getLatestRelease] dir : ', dir)
   const versionsDesc = fs.readdirSync(dir).filter((file) => {
     const filePath = path.join(dir, file);
@@ -44,6 +50,11 @@ let getBaseUrl = () => {
     return 'http://download.appname.com' // TODO: Change This to actual URL
   }
 };
+
+// APP 기본 URL
+app.get('/', function (req, res) {
+  res.sendfile(path.join(__dirname  + '/index.html'))
+});
 
 app.listen(process.env.PORT, () =>{
   console.log(`Express server listening on port ${process.env.PORT}`);
